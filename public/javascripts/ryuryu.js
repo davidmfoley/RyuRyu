89b73321b030a3b2050479cb96429cc014e3a365
Dave Foley <davidmfoley@gmail.com>
2010-01-27 09:58:16 -0800

    1x1, 2x2 json rendering: classes are being set.

--- public/javascripts/ryuryu.js
+++ public/javascripts/ryuryu.js
@@ -4,7 +4,74 @@ ryuryu.board = function(element) {
 	var wrapper = $(element);
 	return {
 		load : function(json) {
-			wrapper.html("<div class='square'/>");
+			wrapper.html('');
+			var squares = {};
+			var regions = eval(json);
+
+			var squareMarkup = "";
+
+			square_count = 0;
+
+			for (var i = 0; i < regions.length; i++) {
+				var region = regions[i];
+				for (var j = 0; j < region.squares.length; j++) {
+
+					var square = region.squares[j];
+					var info;
+
+					if (j == 0)
+						info = '' + region.total + region.operator;
+					else
+						info = "";
+
+					squares[square] = {
+						info : info,
+						region : region
+					};
+					square_count++;
+				}
+			}
+
+			var board_size = Math.sqrt(square_count);
+
+			for (var row = 1; row <= board_size; row++) {
+				for (var col = 1; col <= board_size; col++) {
+					var s = squares[[row, col]];
+
+					squareMarkup += "<div class='square" + getSquareClasses(s, row, col, board_size) + "'><div class='region-info'>" + s.info + "</div></div>";
+				}
+			}
+
+			console.log(squareMarkup);
+
+			wrapper.html(squareMarkup);
 		}
 	};
+
+	function getSquareClasses(square, row, col, board_size) {
+
+		var hasRightNeighbor =false, hasBottomNeigbor = false;
+
+		for (var i = 0; i < square.region.squares.length; i++) {
+			var other = square.region.squares[i];
+
+			if (other[0] == row+1 && other[1] == col)
+				hasBottomNeigbor = true;
+			if (other[0] == row && other[1] == col+1)
+				hasRightNeighbor = true;
+		
+			console.log(row, col, hasBottomNeigbor, hasRightNeighbor);
+		}
+
+		return	classIf( 'right-edge', (col < board_size) && !hasRightNeighbor) +
+			classIf( 'bottom-edge', (row < board_size) && !hasBottomNeigbor) +
+			classIf( 'outside-top-edge', row == 1) +
+			classIf( 'outside-left-edge', col == 1) +
+			classIf( 'outside-bottom-edge', row == board_size) +
+			classIf( 'outside-right-edge', col == board_size);
+	}
+
+	function classIf( className, condition) {
+		return (condition) ? " " + className  : "";
+	}
 };
--- public/javascripts/specs/board_spec.js
+++ public/javascripts/specs/board_spec.js
@@ -1,21 +1,96 @@
-
-
-
 Screw.Unit(function() {
-	describe('rendering json', function() {
+	describe('loading board from json', function() {
 		describe('for a simple 1x1 board', function() {
 			var board;
 			before(function() {
 				$('#sandbox').html('<div id="board-wrapper"></div>"');
 				board = ryuryu.board($('#board-wrapper'));
 
-				board.load({squares : [
-					[1,1]
-				]});
+				board.load([
+					{
+						squares : [
+							[1,1]
+						],
+						total : 1,
+						operator : "+"
+					}
+				]);
 			});
+
 			it('should have a square', function() {
 				expect($('.square').length).to(equal, 1);
 			});
+			it('should have the total and operator', function() {
+				expect($('.square > .region-info').text()).to(equal, '1+');
+			});
+
+			it('should have all four outer edges set', function() {
+				var square = $('.square');
+
+				expect(square).to(match_selector, ".outside-top-edge" );
+				expect(square).to(match_selector, ".outside-bottom-edge" );
+				expect(square).to(match_selector, ".outside-left-edge" );
+				expect(square).to(match_selector, ".outside-right-edge" );
+			});
+
+			it('should not have region edges', function() {
+				expect($('.square.right-edge').length).to(equal, 0);
+				expect($('.square.bottom-edge').length).to(equal, 0);
+			});
+		});
+
+
+		describe('for a 2x2 board', function() {
+			var board;
+			before(function() {
+				$('#sandbox').html('<div id="board-wrapper"></div>"');
+				board = ryuryu.board($('#board-wrapper'));
+
+				board.load([
+					{
+						squares : [
+							[1,1]
+						],
+						total : 1,
+						operator : "+"
+					},
+					{
+						squares : [
+							[1,2],
+							[2,1],
+							[2,2]
+						],
+						total : 4,
+						operator : "*"
+					}
+				]);
+			});
+
+			it('should have four squares', function() {
+				expect($('.square').length).to(equal, 4);
+			});
+
+			it('should display the total and operator in the top-left square', function() {
+				expect($('.square').first().text()).to(equal, "1+");
+			});
+
+			it('should display the total and operator in the second square', function() {
+				expect($('.square').slice(1).first().text()).to(equal, "4*");
+			});
+
+			it('should not display total or operator in the other two squares', function() {
+				expect($('.square').slice(2,3).text()).to(equal, "");
+			});
+
+			it('should set region edges on the top-left square', function() {
+				expect($('.square').first()).to(match_selector, ".right-edge.bottom-edge");
+			});
+
+			it('should not set region edges on any other squares', function() {
+				expect($('.square.right-edge').length).to(equal, 1);
+				expect($('.square.bottom-edge').length).to(equal, 1);
+			});
 		});
 	});
+
 });
